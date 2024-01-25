5549558bda774193101f189da800de60bd0c3bcf
Joao Henrique Albuquerque <joaohccalbu@gmail.com>
2024-01-24 22:26:36 -0300

    🔊 logs(headers): trabalhando com req headers

--- src/server.js
+++ src/server.js
@@ -9,15 +9,33 @@ import http from "node:http";
 //PATCH => Atualizar uma info específica no back-end
 //DELETE => Deletar um recurso no backend
 
+//GET /users => Lista os usuários
+//POST /users => Lista os usuários
+
+//Statefull (salva em memória) - Stateless (salva em banco)
+
+//JSON - JavaScript Object Notation
+
+//HEADERS - são metadadosque adicionam contexto aos dados transitados nos requests
+
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === "GET" && url === "/users") {
-    return res.end("Listagem de usuários");
+    res.setHeader("content-type", "application/json");
+    return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    return res.end("Criação de usuários");
+    users.push({
+      id: 1,
+      name: "Fulano de Tal",
+      email: "fulanodetal@examplo.com",
+    });
+
+    return res.end(JSON.stringify(users));
   }
 
   return res.end("Hello Code 2");
