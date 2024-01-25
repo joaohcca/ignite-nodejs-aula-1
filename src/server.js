7efbdd037318503e83c3e7dd7d0d5818820684bc
Joao Henrique Albuquerque <joaohccalbu@gmail.com>
2024-01-24 22:18:33 -0300

    🔊 logs(req): adiciona log de method e url

--- src/server.js
+++ src/server.js
@@ -1,6 +1,25 @@
 import http from "node:http";
 
+// HTTP Methods
+//GET, POST, PUT, PATCH, DELETE
+
+//GET => Buscar um recurso no backend
+//POST => Criar um recurso no backend
+//PUT => Atualizar um recurso no backend
+//PATCH => Atualizar uma info específica no back-end
+//DELETE => Deletar um recurso no backend
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res.end("Listagem de usuários");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return res.end("Criação de usuários");
+  }
+
   return res.end("Hello Code 2");
 });
 
